39c76e0208772489a84ec9f2bf6d91e065af69fd
ZanY3 <146362968+ZanY3@users.noreply.github.com>
2024-04-03 15:38:25 +0300

    level 2 start

--- Assets/Scripts/HoldButton.cs
+++ Assets/Scripts/HoldButton.cs
@@ -8,17 +8,31 @@ public class HoldButton : MonoBehaviour
     public AudioClip clickSound;
     public Transform buttonObj; // to do on, off anim
     public GameObject wallOnButton;
+    public bool isLadder = false;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         source.PlayOneShot(clickSound);
         buttonObj.position = new Vector3(buttonObj.position.x, -4.795f, buttonObj.position.z);
         wallOnButton.SetActive(false);
+        if (isLadder)
+        {
+            source.PlayOneShot(clickSound);
+            buttonObj.position = new Vector3(buttonObj.position.x, -4.795f, buttonObj.position.z);
+            wallOnButton.SetActive(true);
+        }
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
         source.PlayOneShot(clickSound);
         buttonObj.position = new Vector3(buttonObj.position.x, -4.661f, buttonObj.position.z);
         wallOnButton.SetActive(true);
+        if (isLadder)
+        {
+            source.PlayOneShot(clickSound);
+            buttonObj.position = new Vector3(buttonObj.position.x, -4.795f, buttonObj.position.z);
+            wallOnButton.SetActive(false);
+        }
     }
+   
 }
